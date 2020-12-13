7504ee7ed175a4fd4ed17e352b5504969bf7f79c
Przemek <przem.nowosada@gmail.com>
2020-12-13 10:26:13 +0100

    Exercise-11-13_3SDA

---
+++ src/main/java/exercises3sda_11_13/AreaCounter.java
@@ -0,0 +1,19 @@
+package exercises3sda_11_13;
+
+import java.util.List;
+
+public class AreaCounter {
+    public static Double countArea(List<Figure>figures){
+        Double sumArea=0.0;
+        for(Figure figure: figures){
+            sumArea=sumArea+figure.getArea();
+        }
+        return sumArea;
+    }
+    public static boolean checkArea(double area,List<Figure>figures){
+        Double result = countArea(figures);
+
+        return result<=a ;
+
+    }
+}
--- src/main/java/exercises3sda_11_13/Main.java
+++ src/main/java/exercises3sda_11_13/Main.java
@@ -1,5 +1,8 @@
 package exercises3sda_11_13;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Circle circle=new Circle(2);
@@ -8,6 +11,18 @@ public class Main {
         System.out.println("Pole prostokąta:"+rectangle.getArea());
         Triangle triangle=new Triangle(1.1,1.3);
         System.out.println("Pole trójkąta:"+triangle.getArea());
+
+        List<Figure>figures=new LinkedList<>();
+        figures.add(rectangle);
+        figures.add(circle);
+        figures.add(triangle);
+
+        double result=AreaCounter.countArea(figures);
+        System.out.println(result);
+
+        double area=300.0;
+        boolean isEnough= AreaCounter.checkArea(area,figures);
+        System.out.println("Feedback if the amount "+isEnough);
 
     }
 }
